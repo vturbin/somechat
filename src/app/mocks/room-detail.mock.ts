859edd5988e19c4d542e885e714762585b75d366
Valentin Turbins <v.turbin@hotmail.com>
2022-01-15 15:12:13 +0100

    added plus button to add group

--- src/app/mocks/room-detail.mock.ts
+++ src/app/mocks/room-detail.mock.ts
@@ -82,4 +82,22 @@ export const roomListMock: RoomDetails[] = [
       timestamp: 1640616380425,
     },
   },
+  {
+    id: '2',
+    image: 'covid.jpeg',
+    title: 'Yoyo Dancer',
+    lastMessage: {
+      message: 'kodpwqkodpwkdopwqkdopwkqdopwqk',
+      timestamp: 1640616380425,
+    },
+  },
+  {
+    id: '3',
+    image: 'covid.jpeg',
+    title: 'Убийцы ковида',
+    lastMessage: {
+      message: 'kodpwqkodpwkdopwqkdopwkqdopwqk',
+      timestamp: 1640616380425,
+    },
+  },
 ];
